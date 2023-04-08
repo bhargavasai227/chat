9d81367849ca67b6dfa297e73851260461ab657c
bhargavasai227 <112853876+bhargavasai227@users.noreply.github.com>
2023-04-08 16:48:38 +0530

    hi2

--- src/components/messages.jsx
+++ src/components/messages.jsx
@@ -5,8 +5,15 @@ import Msg from './msg'
 function messages() {
     return (
         <div className='bg-[#379683] h-[90vh] rounded-md flex justify-between flex-col' >
-            <div>
+            <div className='whitespace-nowrap overflow-auto scrollbar-hide '>
                 <Msg />
+                <Msg />
+                <Msg />
+                <Msg />
+                <Msg />
+                <Msg /><Msg />
+                <Msg />
+                <Msg /><Msg />
                 <Msg />
                 <Msg />
             </div>
--- src/components/msg.jsx
+++ src/components/msg.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 function msg() {
+    const time = new Date().toLocaleTimeString('en-GB', {
+        hour: "numeric",
+        minute: "numeric"
+    });
     return (
-        <div className='bg-[#8ee4af] mx-3 mb-2 flex justify-between px-2'>
+        <div className='bg-[#8ee4af] mx-3 mb-2 flex justify-between px-3 rounded-md'>
             <div>
-                <img src='#' alt='dp'></img>
-                <span>time</span>
+                <img src='http://ecap.pace.ac.in//StudentPhotos/21kq1a0545.jpg' alt='dp' className=' h-8'></img>
+                <p className='text-xs'>{time}</p>
             </div>
-            <div>message</div>
+            <div className='pr-3'>message</div>
         </div>
     )
 }
